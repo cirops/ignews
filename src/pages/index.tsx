8063acfff4662d0763bdb38086d464e972efc16b
Ciro Silva <cirops@gmail.com>
2022-03-27 21:29:12 -0300

    feat: adds server side rendering for the subscription product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import avatar from "../../public/images/avatar.svg";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -21,10 +28,10 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90/month</span>
+            <span>for {product.amount}/month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image
@@ -37,3 +44,20 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const price = await stripe.prices.retrieve(process.env.STRIPE_PRICE_ID);
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "usd",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+  };
+};
